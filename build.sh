96621f8613bdf00ab1e69b73aac50db9008c2774
ifekel <ifeanyionyekwelu786@gmail.com>
2023-11-05 00:22:44 +0100

    Pre-hosting commit

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ set -o errexit
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
-python manage.py makemigrations glamourApp
+python manage.py makemigrations app
 python manage.py makemigrations users
 python manage.py migrate
